94f809692c807e8e4661de573a21e45d2d3049aa
Nicole Lopez <nmlopez21@gmail.com>
2014-11-02 21:59:59 -0600

    attempting to change display of button if checked

--- app.js
+++ app.js
@@ -130,49 +130,65 @@ $questions = $('#questions');
 
 function displayQuestions(questionsList, onPage){ 
   for (var i = 0; i < questionsList.items.length; i++) {
-  // display question
-  if (questionsList.items[i].onPage == onPage){
-    // I added a class to each question with the name of th equestion 
-  var $container = $('<div ' + 'class=' + '"' + questionsList.items[i].onPage + '"' + '>');
-  var $Qdisplay = $('<p>' + questionsList.items[i].question + '</p>');
-  $container.append($Qdisplay);
-  var answers = questionsList.items[i].possibleAnswers
-  for (x in answers) {
-    var $Adisplay = $('<div>');
-    $Adisplay.addClass('Aoption');
-    if (questionsList.items[i].inputType === "radio") {
-      // Added a value to each radio answer to allow the serializeArray to pick up which answer the user marks.
-      var $Aoption = $('<input type= '+'"'+ questionsList.items[i].inputType +'"'+'name = ' + '"'+ questionsList.items[i].name+'"' + 'value = '+ '"' + answers[x] + '"' + 'class= '+'"' +answers[x]+'"'+ '/>' + '<label>' + answers[x] + '</label>');
-      //Added radioButton class to radio answers to help w/ custom radios and checkboxes -- NL 11/02
-      $Aoption.addClass("radioButton");   
-    }
-    else {
-      var $Aoption = $('<input type= '+'"'+ questionsList.items[i].inputType +'"'+'name = ' + '"'+ answers[x]+'"' + 'class= '+'"' +answers[x]+'"'+ '/>' + '<label>' + answers[x] + '</label>');
-      //Added checkboxButton class to help w/ custom stylings of checkboxes
-      $Aoption.addClass("checkboxButton"); 
-    }
-    $Adisplay.append($Aoption);
-    $container.append($Adisplay);
-  }
-  
-  $questions.append($container);
-};
-};
+    // display question
+    if (questionsList.items[i].onPage == onPage){
+      // I added a class to each question with the name of th equestion 
+      var $container = $('<div ' + 'class=' + '"' + questionsList.items[i].onPage + '"' + '>');
+      var $Qdisplay = $('<p>' + questionsList.items[i].question + '</p>');
+      $container.append($Qdisplay);
+      var answers = questionsList.items[i].possibleAnswers
+      for (x in answers) {
+        var $Adisplay = $('<div>');
+        $Adisplay.addClass('Aoption');
+        if (questionsList.items[i].inputType === "radio") {
+          // Added a value to each radio answer to allow the serializeArray to pick up which answer the user marks.
+          var $Aoption = $('<input type= '+'"'+ questionsList.items[i].inputType +'"'+'name = ' + '"'+ questionsList.items[i].name+'"' + 'value = '+ '"' + answers[x] + '"' + 'class= '+'"' +answers[x]+'"'+ '/>' + '<label>' + answers[x] + '</label>');
+            //Added radioButton class to radio answers to help w/ custom radios and checkboxes -- NL 11/02
+          $Aoption.addClass("radioButton");   
+        }
+        else {
+          var $Aoption = $('<input type= '+'"'+ questionsList.items[i].inputType +'"'+'name = ' + '"'+ answers[x]+'"' + 'class= '+'"' +answers[x]+'"'+ '/>' + '<label>' + answers[x] + '</label>');
+          //Added checkboxButton class to help w/ custom stylings of checkboxes
+          $Aoption.addClass("checkboxButton"); 
+        }
+        $Adisplay.append($Aoption);
+        $container.append($Adisplay);
+      }
+      $questions.append($container);
+    };
+  };
 }
 
 displayQuestions(questionsList, "startPage");
 
+
+//================DO WE STILL NEED THE BELOW CODE?==============
 // This is just to show the basic version of how I collected the results from the radio and checkboxes  
 // I changed the name so that the radio buttons would be exclusive -- NL  
-$("input:radio[name='Individual']").click(function() {
-    var value = $(this).val();
-    console.log(value);
-});
+// $("input:radio[name='Individual']").click(function() {
+//     var value = $(this).val();
+//     console.log(value);
+// });
+//================end===========================================
 
 
 
 // Below:  This can probably be refractored into a resuable function.  
 // It monitors for a change in the .Food answer and checks if it is checked.  If checked the isFood questions display.  If not (!) checked, it removes the isFood questions.  
+
+
+//TRYING TO ADD CLASS TO BLANK BUTTON IF CHECKED
+//NOT WORKING -- NL 11/2
+$('.radioButton').change(function() {
+    if (this.checked){
+    console.log("hi");
+      //displayQuestions(questionsList, "isFood");
+} else if (!this.checked){
+  console.log("bye");
+  //$('.isFood').remove();
+}
+}); 
+
 
 $('.Food').change(function() {
     if (this.checked){
