7cf101edc0709b832642e5315c32984a91275046
CorainChicago <chaysmagan@gmail.com>
2014-12-03 00:59:07 -0600

    Deleted extra console.logs

--- app.js
+++ app.js
@@ -276,8 +276,7 @@ for (var i = 0; i < resultsJSON.length; i++) {
       };
 
     if(resultsJSON[i].value == "Individual" && resultsJSON[i].name == "isPerishable" && resultsJSON[i].value == "isPerishable"|| resultsJSON[i].name == "needRefrigerator" & resultsJSON[i].value == "Yes") {
-        console.log("value" + resultsJSON[i].value);
-        console.log("name" + resultsJSON[i].name);
+       
         var $exceptionMessage = $('<p>Due to food due to food safety regulations, the pantry cannot accept perishable or food needing refrigeration from individuals.</p>');
         $resultsDisplay.append($exceptionMessage); 
            // };
